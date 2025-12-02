2b3c90afb3c72a3ecdc2d91dbb77439a9d412612
yuizho <yuizho3@gmail.com>
2025-12-03 00:28:20 +0900

    move Template codes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Template from './components/basics/Template.vue'
 
 const plainMsg = 'Welcome to Your Vue.js App'
 const dynamicId = 'welcome-message'
@@ -29,21 +30,7 @@ const eventName = 'mouseover'
   </header>
 
   <main>
-    <h1>基本</h1>
-    <h2>テンプレート構文</h2>
-    <p :id="dynamicId">Message: {{ plainMsg }}</p>
-    <p v-html="rawHtml"></p>
-    <button :disabled="isDisabled">Click me</button>
-    <p v-bind="objectOfAttrs">multiple attrs are configured by v-bind</p>
-    <p v-if="seen">Now you see me</p>
-    <p v-else>Now you don't</p>
-    <button @click="showAlert('click')">Show Alert</button>
-    <br />
-    <a :[attributeName]="url">Vue Official Website</a>
-    <br />
-    <button v-on:[eventName]="showAlert(eventName)">Show Alert via v-on:mouseover</button>
-    <br />
-    <input @keyup.enter="showAlert('enter key')" placeholder="Press Enter" />
+    <Template />
   </main>
 </template>
 
