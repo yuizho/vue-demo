0ca89231cfc7149f7dee8f297b047ecd27cc2386
yuizho <yuizho3@gmail.com>
2025-12-09 01:08:40 +0900

    move Basics

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,9 @@
 <script setup>
-import { ref } from 'vue'
-import Template from './components/basics/Template.vue'
-import Reactivity from './components/basics/Reactivity.vue'
-import Computed from './components/basics/Computed.vue'
-import ClassStyleBinding from './components/basics/ClassStyleBinding.vue'
-import RenderingWithCondition from './components/basics/RenderingWithCondition.vue'
-import ListRendering from './components/basics/ListRendering.vue'
-import EventHandling from './components/basics/EventHandling.vue'
-import FormBinding from './components/basics/FormBinding.vue'
-import Watcher from './components/basics/Watcher.vue'
-import TemplateReference from './components/basics/TemplateReference.vue'
-import Component from './components/basics/Component.vue'
-
-const plainMsg = 'Welcome to Your Vue.js App'
-const dynamicId = 'welcome-message'
-const rawHtml = '<h2>This is h2 rendered from raw HTML.</h2>'
-const isDisabled = true
-const objectOfAttrs = {
-  id: 'multi-attrs-message',
-  class: 'multi-attrs',
-  style: 'color: blue; font-weight: bold;',
-}
-const seen = true
-const showAlert = (eventName) => {
-  alert(`${eventName} event triggered!`)
-}
-const attributeName = 'href'
-const url = 'https://vuejs.org/'
-const eventName = 'mouseover'
-
-const componentFontSize = ref(1)
+import Basics from './components/basics/Basics.vue'
 </script>
 
 <template>
   <main>
-    <h1>基本</h1>
-    <Template />
-    <Reactivity />
-    <Computed />
-    <ClassStyleBinding />
-    <RenderingWithCondition />
-    <ListRendering />
-    <EventHandling />
-    <FormBinding />
-    <Watcher />
-    <TemplateReference />
-    <div :style="{ fontSize: componentFontSize + 'em' }">
-      <Component
-        title="Hello Vue!"
-        @enlarge-text="componentFontSize += 0.1"
-        @shrink-text="componentFontSize -= 0.1"
-      >
-        <p>これは親から渡したテキストです。</p>
-      </Component>
-    </div>
+    <Basics />
   </main>
 </template>
-
-<style scoped>
-main {
-  padding: 1rem;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
-</style>
---
+++ src/components/basics/Basics.vue
@@ -0,0 +1,46 @@
+<script setup>
+import Template from './Template.vue'
+import Reactivity from './Reactivity.vue'
+import Computed from './Computed.vue'
+import ClassStyleBinding from './ClassStyleBinding.vue'
+import RenderingWithCondition from './RenderingWithCondition.vue'
+import ListRendering from './ListRendering.vue'
+import EventHandling from './EventHandling.vue'
+import FormBinding from './FormBinding.vue'
+import Watcher from './Watcher.vue'
+import TemplateReference from './TemplateReference.vue'
+import Component from './Component.vue'
+
+import { ref } from 'vue'
+const componentFontSize = ref(1)
+</script>
+
+<template>
+  <h1>基本</h1>
+  <Template />
+  <Reactivity />
+  <Computed />
+  <ClassStyleBinding />
+  <RenderingWithCondition />
+  <ListRendering />
+  <EventHandling />
+  <FormBinding />
+  <Watcher />
+  <TemplateReference />
+  <div :style="{ fontSize: componentFontSize + 'em' }">
+    <Component
+      title="Hello Vue!"
+      @enlarge-text="componentFontSize += 0.1"
+      @shrink-text="componentFontSize -= 0.1"
+    >
+      <p>これは親から渡したテキストです。</p>
+    </Component>
+  </div>
+</template>
+
+<style scoped>
+main {
+  padding: 1rem;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
