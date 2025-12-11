5094e742a8e1f2dd889cb40fb01f60a5dea573be
yuizho <yuizho3@gmail.com>
2025-12-11 23:59:05 +0900

    add v-model demo

--- src/components/component_details/ComponentDetails.vue
+++ src/components/component_details/ComponentDetails.vue
@@ -1,10 +1,14 @@
 <script setup>
+import { ref } from 'vue'
 import Props from './Props.vue'
 import Event from './Event.vue'
+import VModel from './VModel.vue'
+
 const title = 'コンポーネントのタイトル'
 const callback = (where) => {
   alert(`${where}でイベントが発火しました`)
 }
+const countModel = ref(0)
 </script>
 
 <template>
@@ -12,5 +16,6 @@ const callback = (where) => {
     <h1>コンポーネントの詳細</h1>
     <Props :title="title" />
     <Event @some-event="callback" />
+    <VModel v-model="countModel" v-model:title="title" />
   </div>
 </template>
---
+++ src/components/component_details/VModel.vue
@@ -0,0 +1,18 @@
+<script setup>
+const model = defineModel()
+const title = defineModel('title', { required: true })
+
+function update() {
+  model.value++
+}
+</script>
+
+<template>
+  <div>
+    <h2>v-model</h2>
+    <div>Parent bound v-model is: {{ model }}</div>
+    <button @click="update">Increment from Child</button>
+    <br />
+    <input v-model="title" />
+  </div>
+</template>
