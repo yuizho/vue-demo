7225fd625a462b798c93ed1e1dfa01b39a168ef0
yuizho <yuizho3@gmail.com>
2025-12-11 23:30:58 +0900

    add event demo

--- src/components/component_details/ComponentDetails.vue
+++ src/components/component_details/ComponentDetails.vue
@@ -1,11 +1,16 @@
 <script setup>
 import Props from './Props.vue'
+import Event from './Event.vue'
 const title = 'コンポーネントのタイトル'
+const callback = (where) => {
+  alert(`${where}でイベントが発火しました`)
+}
 </script>
 
 <template>
   <div>
     <h1>コンポーネントの詳細</h1>
     <Props :title="title" />
+    <Event @some-event="callback" />
   </div>
 </template>
---
+++ src/components/component_details/Event.vue
@@ -0,0 +1,11 @@
+<script setup>
+// events can define explicitly
+defineEmits(['someEvent'])
+</script>
+
+<template>
+  <div>
+    <h2>イベント</h2>
+    <button @click="$emit('someEvent', '子供')">イベントを発火</button>
+  </div>
+</template>
