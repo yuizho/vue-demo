d24c7d400d504249e21944442acb4f4ed0bbb697
yuizho <yuizho3@gmail.com>
2025-12-16 01:17:17 +0900

    add custome directive demo

---
+++ src/components/reusability/CustomDirective.vue
@@ -0,0 +1,24 @@
+<script setup>
+// カスタムディレクティブはDOMを直接操作しないと行けないときだけに使うこと！！
+// https://ja.vuejs.org/guide/reusability/custom-directives.html#when-to-use
+
+// 接頭辞が v で始まるキャメルケースの変数をカスタムディレクティブとして使用することができる
+const vHighligt = {
+  mounted: (el) => {
+    el.classList.add('is-highlight')
+  },
+}
+</script>
+
+<template>
+  <div>
+    <h2>カスタムディレクティブ</h2>
+    <p v-highligt>このテキストはハイライトされています。</p>
+  </div>
+</template>
+
+<style scoped>
+.is-highlight {
+  background-color: orange;
+}
+</style>
--- src/components/reusability/Reusability.vue
+++ src/components/reusability/Reusability.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Composable from './Composable.vue'
+import CustomDirective from './CustomDirective.vue'
 </script>
 
 <template>
   <div>
     <h1>再利用性</h1>
     <Composable />
+    <CustomDirective />
   </div>
 </template>
